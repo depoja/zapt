719c684b839c9e6c09a6de1ea96a84be179c4047
Klint Mane <klintmane@gmail.com>
2020-09-09 18:01:23 +0200

    Add examples folder

---
+++ examples/auth.ts
@@ -0,0 +1,35 @@
+import rawy, { PluginFn } from "../lib";
+
+const authenticate: PluginFn = () => {
+  const account = undefined; // Get from cookies or smth;
+
+  return (req, res) => {
+    req.state("account", account);
+  };
+};
+
+const protect: PluginFn = () => {
+  return (req, res) => {
+    if (!req.state("account")) {
+      throw new Error("You are not authenticated");
+    }
+  };
+};
+
+const app = rawy();
+
+const protectedRoutes: PluginFn = (app) => {
+  app.use(protect);
+
+  app.get("/protected", (req, res) => {
+    return "This route is protected";
+  });
+};
+
+app.use(authenticate).use(protectedRoutes);
+
+app.get("/", (req, res) => {
+  return "This one is unprotected";
+});
+
+app.listen(3000);
---
+++ examples/plugins.ts
@@ -0,0 +1,36 @@
+import rawy, { PluginFn } from "../lib";
+
+const app = rawy();
+
+// Plugin declaration
+const log: PluginFn = () => {
+  console.log("Setting up plugin");
+
+  return (res, req) => {
+    console.log("Request");
+  };
+};
+
+// Plugin registration
+app.use(log); // Whenever a request is made, the log plugin will be called, then the rest (get route below)
+
+app.get("/logs", (req, res) => {
+  return "Check your terminal";
+});
+
+// Nested plugin
+const greet: PluginFn = () => (res, req) => {
+  console.log("Hello");
+};
+
+const nested: PluginFn = (a) => {
+  a.use(greet); // Only applied to the current scope ('a'), will be called only for the items declared by it
+
+  a.get("/greet", (req, res) => {
+    return "Check your terminal, both log and greet";
+  });
+};
+
+app.use(nested);
+
+app.listen(3000);
