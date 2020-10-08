ee68c71369ec8ff0073a2f797c7bf1b44bdbad4d
Klint Mane <klintmane@gmail.com>
2020-10-08 16:24:11 +0200

    Plugin typing fixes

--- plugins/cors/opts.ts
+++ plugins/cors/opts.ts
@@ -1,19 +1,26 @@
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 
-export const all = {
-  allowedOrigins: ["*"], // List of origins from where a cross-domain request can be executed. (i.e.: http://*.domain.com),
-  allowedMethods: ["DELETE", "GET", "HEAD", "PATCH", "POST", "PUT"], // List of methods the client is allowed to use with cross-domain requests
-  allowedHeaders: ["*"], // List of headers the client is allowed to use with cross-domain requests
+export const all: Options = {
   allowCredentials: false, // whether the request can include user credentials like cookies, HTTP authentication or client side SSL certificates
-};
-
-export const defaults = {
-  ...all,
-  allowedOrigins: [],
-  allowedMethods: ["GET", "HEAD", "POST"], // Default is spec's "simple" methods - ["GET", "HEAD", "POST"]
-  allowedHeaders: ["Origin", "Accept", "Content-Type", "X-Requested-With"], // CORS-safelisted request headers
+  allowedHeaders: ["*"], // List of headers the client is allowed to use with cross-domain requests
+  allowedMethods: ["DELETE", "GET", "HEAD", "PATCH", "POST", "PUT"], // List of methods the client is allowed to use with cross-domain requests
+  allowedOrigins: ["*"], // List of origins from where a cross-domain request can be executed. (i.e.: http://*.domain.com),
   exposedHeaders: [], // List of headers safe to expose
   maxAge: 60 * 60 * 24, // how long (in seconds) the results of a preflight request can be cached
 };
 
-export type Options = typeof all & typeof defaults;
+export const defaults: Options = {
+  ...all,
+  allowedHeaders: ["Origin", "Accept", "Content-Type", "X-Requested-With"], // CORS-safelisted request headers
+  allowedMethods: ["GET", "HEAD", "POST"], // Default is spec's "simple" methods - ["GET", "HEAD", "POST"]
+  allowedOrigins: [],
+};
+
+export type Options = {
+  allowCredentials: false;
+  allowedHeaders: string[];
+  allowedMethods: string[];
+  allowedOrigins: string[];
+  exposedHeaders: string[];
+  maxAge: number;
+};
